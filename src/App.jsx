b005399fe9bffe68dddd977e80587844c2e2e291
Mateo Bentos <mtbentos01@gmail.com>
2026-05-13 19:52:16 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl'>Hola mundo</h1>
+      <Navbar></Navbar>
       <Footer></Footer>
     </>
   )
